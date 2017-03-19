f0f2be1f8fa0b575c5df4439284d16c6f0003e32
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2017-03-19 18:47:15 +0100

    Vue.js integration

--- src/szeol/szeol/main/views.py
+++ src/szeol/szeol/main/views.py
@@ -1,12 +1,19 @@
 from functools import wraps
 
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.conf import settings
 
 
 def contextwrapper(method):
     @wraps(method)
     def wrapper(self, request, *args, **kwargs):
-        request._context = self._context = getattr(request, '_context', {})
+        context = getattr(request, '_context', None)
+        if not context:
+            context = {
+                'settings': settings,
+            }
+        request._context = self._context = context
         self._request = request
         self._matchdict = kwargs
         self._request.menu_id = getattr(self, 'MENU_ID', '')
@@ -18,6 +25,26 @@ def contextwrapper(method):
             *args)
         if not result:
             return render(request, self.TEMPLATE_NAME, self._context)
+        else:
+            return result
+
+    return wrapper
+
+
+def jsonwrapper(method):
+    @wraps(method)
+    def wrapper(self, request, *args, **kwargs):
+        json = request._json = self._json = getattr(request, '_json', {})
+        self._request = request
+        self._matchdict = kwargs
+        result = method(
+            self,
+            request,
+            json,
+            self._matchdict,
+            *args)
+        if not result:
+            return JsonResponse(json)
         else:
             return result
 
--- src/szeol/szeol/settings/default.py
+++ src/szeol/szeol/settings/default.py
@@ -2,6 +2,7 @@ def make_settings(settings, paths):
     database(settings, paths)
     hosts(settings, paths)
     env(settings, paths)
+    vue(settings, paths)
 
 
 def database(settings, paths):
@@ -21,3 +22,7 @@ def hosts(settings, paths):
 def env(settings, paths):
     settings['SECRET_KEY'] = 'po*sdjb$inb22#zj%%=n)8v7pc!%--%&wah3eqmgvopcc6a#*^o-'
     settings['DEBUG'] = True
+
+
+def vue(settings, paths):
+    settings['VUE_URL'] = 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.4/vue.min.js'
